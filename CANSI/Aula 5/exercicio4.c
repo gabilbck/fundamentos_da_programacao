b674ba695a51080d7ae0205bc7d5c44e58e4bfd4
gadulb <gabrielieduardalembeck@gmail.com>
2024-04-30 21:03:37 -0300

    ADD

--- CANSI/Aula 5/exercicio4.c
+++ CANSI/Aula 5/exercicio4.c
@@ -1,2 +1,24 @@
-/*4. Faça um programa que calcule e mostre a média bimestral da turma.
+// FUNCIONANDO
+/* 4. Faça um programa que calcule e mostre a média bimestral da turma.
 O usuário deve informar a quantidade de notas, bem como cada nota.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    float nota, aux, media = 0;
+    int qtdNotas, i;
+
+    printf("Informe a quantidade de notas: ");
+    scanf("%i", &qtdNotas);
+    fflush(stdin);
+
+    for(i=1; i<qtdNotas; i++){
+        printf("Informe a nota %i: ", i); //digita a nota conforme a quantidade de notas informada
+        scanf("%f", &aux);
+        fflush(stdin);
+        nota = nota+aux;
+    }
+    media = nota/(float)qtdNotas; //calcula a média
+    printf("A média bimestral da turma é: %.2f", media); //mostra a média
+    return 0;
+}
--- CANSI/Aula 5/exercicio5.c
+++ CANSI/Aula 5/exercicio5.c
@@ -1,3 +1,29 @@
+// FUNCIONANDO
 /* 5. Faça um programa que calcule o número médio de alunos por turma. Para isto, 
 peça a quantidade de turmas e a quantidade de alunos para cada turma.
 As turmas não podem ter mais de 40 alunos.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    int alunos, qtdTurmas, aux, i, media = 0;
+
+    printf("Informe a quantidade de turmas: ");
+    scanf("%i", &qtdTurmas);
+    fflush(stdin);
+    for(i=0; i<qtdTurmas; i++){
+        printf("Informe a quantidade de alunos %i: ", i+1);
+        scanf("%i", &aux);
+        fflush(stdin);
+        if(aux>40){
+            printf("\n A turma nao pode ter mais de 40 alunos. ");
+            return 0;
+        }else{
+            alunos = alunos+aux;
+        }
+    }
+    media = alunos/qtdTurmas; //calcula a média
+    printf("A media bimestral da turma e: %i",media); //mostra a média
+    return 0;
+}
