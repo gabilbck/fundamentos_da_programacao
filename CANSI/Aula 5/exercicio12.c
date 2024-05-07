48c499f374c0f6671aeba59993997463cb66c37c
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-05-07 09:43:37 -0300

    ADD

--- CANSI/Aula 5/exercicio12.c
+++ CANSI/Aula 5/exercicio12.c
@@ -1,2 +1,16 @@
 /* 12. Faça um programa que mostre a soma desta serie 
 S = 1/1 + 3/2 + 5/3 + 7/4 + ... + 99/50.*/
+#include <stdio.h>
+
+int main() {
+    float soma = 0;
+
+    // Loop para percorrer os termos da série
+    for (int n = 1; n <= 99; n += 2) {
+        soma += (float)n / (n / 2 + 1);
+    }
+
+    printf("A soma da série é: %.2f\n", soma);
+
+    return 0;
+}
