b188da970ab580d6fb60b6639e9c7be0f7ed5d82
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-05-16 13:16:24 -0300

    Update exercicio6.c

--- CANSI/Aula 5/exercicio6.c
+++ CANSI/Aula 5/exercicio6.c
@@ -33,5 +33,34 @@ int main(void){
 
 //OUTRA VERSAO
 /*
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void){
+	// variáveis
+	int i, qtd_cds;
+	float valor_total = 0, valor_medio, valor_cd;
+	
+	// entrada
+	printf("Digite a quantidade de CDs: ");
+	scanf("%i",&qtd_cds);
+	fflush(stdin);
+	i=1;
+	while(i<=qtd_cds){
+		printf("Digite o valor do cd (%i): ",i);
+		scanf("%f",&valor_cd);
+		fflush(stdin);
+		valor_total+=valor_cd;
+		i++;
+	}
+	
+	// procesamento 
+	valor_medio = valor_total/qtd_cds;
+	
+	// saída
+	printf("A media dos cds e R$ %.2f\n",valor_medio);
+	printf("O total dos cds e R$ %.2f\n",valor_total);
+	
+	return 0;
+}
 */
